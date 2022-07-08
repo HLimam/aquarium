134f6091a49131149e8fd417a48795c297b3c765
PatateHunter <ichtitski@gmail.com>
2022-07-08 14:19:01 +0200

    get fish with fuzzy matching

--- backend/src/main/java/fr/esgi/aquarium/infra/service/SpringFishService.java
+++ backend/src/main/java/fr/esgi/aquarium/infra/service/SpringFishService.java
@@ -1,10 +1,14 @@
 package fr.esgi.aquarium.infra.service;
 
 
+import fr.esgi.aquarium.domain.service.StringSearchMatching;
 import fr.esgi.aquarium.infra.web.response.fish.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -12,8 +16,15 @@ public class SpringFishService {
     private final RestTemplate restTemplate;
 
     public Root[] findByName(String name) {
-        var fishResponse = restTemplate.getForObject("https://www.fishwatch.gov/api/species/{fishName}", Root[].class, name);
+        var response = this.findAll();
+
+        var fishNames = Arrays.stream(response).map(fish -> fish.speciesName).collect(Collectors.toList());
+        var fishNameWithFuzzyMatching = fishNames.stream().filter((fishName)->StringSearchMatching.isFuzzyMatching(fishName,name,60)).collect(Collectors.joining(","));;
+
+
+        var fishResponse = restTemplate.getForObject("https://www.fishwatch.gov/api/species/{fishName}", Root[].class, fishNameWithFuzzyMatching);
         return fishResponse;
+
     }
 
     public Root[] findAll() {
